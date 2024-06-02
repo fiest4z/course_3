66168ccd3d80fdfe378a5fbb73e234ecf41a8ba2
Mikhail Belyaev <fiestaz1337@gmail.com>
2024-06-02 18:51:12 +0300

    add import last_five

--- main/func.py
+++ main/func.py
@@ -3,14 +3,24 @@ import json
 
 
 def import_operations():
+    '''
+    Импорт операций из operations.json
+    '''
     with open('operations.json', 'r', encoding='utf-8') as file:
         operations = json.load(file)
         return operations
 
 
 def convert_time():
+    pass
 
 
+def five_last_operations(operations):
+    '''
+    Последние 5 операций
+    '''
+    sorted_operations= sorted(operations, key=lambda x: x.get('date', '0'))
+    last_five = sorted_operations[:6]
+    return last_five
 
-def five_last_operations():
 
